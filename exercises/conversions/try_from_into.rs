1c31531133e0dfd8d06ae3f8a04712581fbc4032
olalalao <1176328868@qq.com>
2024-10-09 17:10:54 +0800

    update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,7 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -37,10 +36,15 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
-// Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        if 0 <= red && red <= 255 && 0 <= blue && blue <= 255 && 0 <= green && green <= 255 {
+            Ok(Color { red: red as u8, green: green as u8, blue: blue as u8 })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -48,6 +52,12 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let (red, green, blue) = (arr[0], arr[1], arr[2]);
+        if 0 <= red && red <= 255 && 0 <= blue && blue <= 255 && 0 <= green && green <= 255 {
+            Ok(Color { red: red as u8, green: green as u8, blue: blue as u8 })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -55,6 +65,17 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(IntoColorError::BadLen)
+        } else {
+            let (red, green, blue) = (slice[0], slice[1], slice[2]);
+            if 0 <= red && red <= 255 && 0 <= blue && blue <= 255 && 0 <= green && green <= 255 {
+                Ok(Color { red: red as u8, green: green as u8, blue: blue as u8 })
+            } else {
+                println!("Color: {} {} {}", red, green, blue);
+                Err(IntoColorError::IntConversion)
+            }
+        }
     }
 }
 
